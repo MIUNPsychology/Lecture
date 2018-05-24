6ea9eab273a744e530fef2026c51e9e515871e38
Joel Palmius <joepal1976@hotmail.com>
2018-05-24 13:55:46 +0200

    Create main menu and transition to first scene

---
+++ Assets/Scripts/KeyPressToFirstScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class KeyPressToFirstScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			print ("Hello");
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("EmptyRoomScene");
+		}
+	}
+}
