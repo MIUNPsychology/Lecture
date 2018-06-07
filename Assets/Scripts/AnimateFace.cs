8777a980a801fd8276b9f14b820d4f90609738e4
Joel Palmius <joepal1976@hotmail.com>
2018-06-07 13:23:02 +0200

    Add third scene, chain scenes

---
+++ Assets/Scripts/AnimateFace.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateFace : MonoBehaviour {
+
+	private float morph = 0.0f;
+
+	public SkinnedMeshRenderer body;
+	public SkinnedMeshRenderer eyebrows;
+	public SkinnedMeshRenderer eyelashes;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("changeMorph", 2.0f, 0.02f);
+	}
+	
+	void changeMorph() {
+
+		if (morph >= 99.0f) {
+			CancelInvoke ("changeMorph");
+		}
+
+		morph = morph + 1.0f;
+
+		if (body != null) {
+			body.SetBlendShapeWeight (0, morph);
+			body.SetBlendShapeWeight (1, morph);
+			body.SetBlendShapeWeight (6, morph);
+			body.SetBlendShapeWeight (7, morph);
+			body.SetBlendShapeWeight (8, morph);
+			body.SetBlendShapeWeight (13, morph / 2);
+			body.SetBlendShapeWeight (14, morph / 2);
+			body.SetBlendShapeWeight (30, morph);
+			body.SetBlendShapeWeight (31, morph);
+			body.SetBlendShapeWeight (45, morph);
+		}
+
+		if (eyebrows != null) {
+			eyebrows.SetBlendShapeWeight (0, morph);
+			eyebrows.SetBlendShapeWeight (1, morph);
+			eyebrows.SetBlendShapeWeight (6, morph);
+			eyebrows.SetBlendShapeWeight (7, morph);
+			eyebrows.SetBlendShapeWeight (8, morph);
+			eyebrows.SetBlendShapeWeight (13, morph);
+		}
+
+		if (eyelashes != null) 
+		{
+			eyelashes.SetBlendShapeWeight (0, morph);
+		}
+	}
+}
