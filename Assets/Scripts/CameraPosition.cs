4cc0bf6262a82812bd697b980f0878d54d223cb7
Joel Palmius <joepal1976@hotmail.com>
2018-06-07 14:13:07 +0200

    Adjust camera vertical position using arrow keys

---
+++ Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition : MonoBehaviour {
+
+	private static float camera_y = 2.92f;
+	private static float camera_x = 0.0f;
+	private static float camera_z = 0.0f;
+
+	// Use this for initialization
+	void Start () {		
+		if (camera_x == 0.0f) {
+			camera_x = Camera.main.gameObject.transform.position.x;
+		}
+		if (camera_z == 0.0f) {
+			camera_z = Camera.main.gameObject.transform.position.z;
+		}
+		Camera.main.gameObject.transform.position = new Vector3 (camera_x, camera_y, camera_z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("up")) {
+			print ("up");
+			camera_y = camera_y + 0.05f;
+			Camera.main.gameObject.transform.position = new Vector3 (camera_x, camera_y, camera_z);
+		}
+		if (Input.GetKeyDown ("down")) {
+			print ("down");
+			camera_y = camera_y - 0.05f;
+			Camera.main.gameObject.transform.position = new Vector3 (camera_x, camera_y, camera_z);
+		}
+	}
+}
